f71cb3e6517eca9ae1868de6ca149b20b2c8032c
de-itsnotme <de.deepesh@outlook.com>
2016-08-15 17:01:50 +0200

    Templates modified + minor changes to the DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Airports;
 
 class DefaultController extends Controller
 {
@@ -39,5 +40,25 @@ class DefaultController extends Controller
         return $this->render('default/login.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @Route("/getAirports")
+     */
+    public function getAirportsAction(Request $request)
+    {
+
+        $value = $request->query->get('getAirports');
+
+        $airports = $this->getDoctrine()
+            ->getRepository('AppBundle:Airports')
+            ->createQueryBuilder('A')
+            ->select(array('A.name', 'A.airlineId'))
+            ->where('A.name LIKE :name')
+            ->setParameter('name','%'.$value.'%')
+            ->setMaxResults(8)
+            ->getQuery()
+            ->getArrayResult();
+        return $this->json($airports);
     }
 }
